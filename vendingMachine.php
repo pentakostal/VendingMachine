f18226e5dc78e19059c69a636fa09a95fb3b42d3
Pavels Skokovs <dstars72@gmail.com>
2022-11-06 21:28:29 +0200

    base working program

--- vendingMachine.php
+++ vendingMachine.php
@@ -1,1 +1,44 @@
 <?php
+declare(strict_types=1);
+//Declare vending machine data base/menu
+function createProduct(string $name, int $price): stdClass
+{
+    $product=new stdClass();
+    $product->name = $name;
+    $product->price = $price;
+    return $product;
+}
+
+$menu = [
+    createProduct("latte", 100),
+    createProduct("cappuccino", 120),
+    createProduct("white", 80),
+    createProduct("black", 70),
+];
+//Choose product
+echo "Choose product:\n";
+echo "0. latte\n";
+echo "1. cappuccino\n";
+echo "2. white\n";
+echo "3. black\n";
+(int) $clientChoice = readline("-> ");
+//Counting
+$wallet = 500;
+$coins = [200, 100, 50, 20, 10, 5, 2, 1];
+$balance = $wallet - $menu[$clientChoice]->price;
+$coinCash = [];
+$i = 0;
+while ($balance > 0 && $i < 8) {
+    $reminder = $balance % $coins[$i];
+    if ($reminder == 0) {
+        $coinCash[$coins[$i]] = ($balance/$coins[$i]);
+        $balance = $balance/$coins[$i];
+        break;
+    } else {
+        $coinCash[$coins[$i]] = (intval($balance/$coins[$i]));
+        $balance = $reminder;
+        $i++;
+    }
+    var_dump($balance);
+}
+var_dump($coinCash);
